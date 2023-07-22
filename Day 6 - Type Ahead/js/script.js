604c4c8d603a09dfa8dc5c23e96a0ba7fa8248de
Mehdi Ouhab <ouhab.me@gmail.com>
2023-07-22 16:08:47 +0200

    Pending

--- Day 6 - Type Ahead/js/script.js
+++ Day 6 - Type Ahead/js/script.js
@@ -1,8 +1,20 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const sortedData = []
+const search = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
 
-async function getData(url){
-    const request = await fetch(url, {method: GET});
-    console.log(request)
-}
+fetch(endpoint)
+.then(response => response.json())
+.then(data => {
+    data.forEach(city => {
+        sortedData.push([city.city, city.state, city.population]);
+    })
+})
 
-getData(endpoint)
+search.addEventListener("input", (e) => {
+    suggestions.innerHTML = "";
+    let preview = sortedData.filter(city => city[0].toLowerCase().includes(`${e.target.value.toLowerCase()}`) || city[1].toLowerCase().includes(`${e.target.value.toLowerCase()}`));
+    preview.forEach(suggestion => {
+        suggestions.innerHTML += `<li>${suggestion[0]}, ${suggestion[1]} <span>${suggestion[2]}</span></li>`
+    })
+})
